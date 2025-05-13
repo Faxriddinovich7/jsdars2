eebaf053627e6012c297f508f02459e55ffdee0e
usmonovsanjar <sanjarusmonov231@gmail.com>
2025-05-13 05:10:56 -0700

    jsdars2

---
+++ jsdars.js
@@ -0,0 +1,89 @@
+// let yosh =prompt("What is your age?");
+// if (yosh <= 18) {
+//     console.log("siz xali yoshsiz ukam ");
+// }else {
+//     console.log("siz 18 yoshdan kattasiz bosing brat");
+// }
+
+// let raqam =prompt("enter one number?");
+// if (raqam*1 === 0 ) {
+//     console.log("Bu nol");
+// }else if(raqam * 1 % 2 === 0) {
+//     console.log("Bu musbat son");
+// }else{
+//     console.log("Bu manfiy son");
+// }
+
+// let parol =prompt("enter password?");
+// if (parol == "manfaat123") {
+//     console.log("xush kelibsiz ");
+// }else{
+//     console.log("ukam keyngi safar yaxshilab tayyorlanib kel");
+// }
+
+// let baho =prompt("enter the score?");
+// if (90 <= baho ) {
+//     console.log("Alo!!!");
+// }else if (70 <= baho && baho <= 89) {
+//     console.log("Yaxshi Ahmad!!!");
+// }else if (50 <= baho && baho <= 69) {
+//     console.log("Qoniqarli!!!");
+// }else if (baho <= 50) {
+//     console.log("Harry!!!");
+// }
+
+// let weather =prompt("what is weather?");
+// if ( 30 <= weather) {
+//     console.log("havo issib ketti uje uyga chop");
+// }else if (15 <= weather && weather <= 30 ) {
+//     console.log("Ob-havo yaxshi kochaga chiq mujik");
+// }else if (0 <= weather && weather <= 15 ) {
+//     console.log("havo sovub ketti kurtkani kiy")
+// }
+
+// let kun = prompt(" hafta kunni kriting");
+// if (kun === "shanba" || kun ==="yakshanba") {
+//     console.log("dam olish kuni")
+// }else if (
+//     kun === "dushanba"||
+//     kun === "seshanba"||
+//     kun === "chorshanba"||
+//     kun === "payshanba"||
+//     kun === "juma"
+// ) {
+//     console.log("ish kuni")
+// }else {
+//     console.log("buna kun yuq2")
+// }
+
+// let a =prompt("first number")
+// let b =prompt("second number")
+// let c =prompt("third number")
+// if ( a > b ){
+//     console.log( a + "katta son")
+// }else if ( b > c ){
+//     console.log( b + "katta son")
+//
+// }else if ( c > b ){
+//     console.log( c + "katta son")
+// }else {
+//     console.log("sonlar teng")
+// }
+
+// let a =prompt("first corner")
+// let b =prompt("second corner")
+// let c =prompt("third corner")
+// if(a===b && b===c){
+//     console.log("Teng tomonli uchburchak")
+// }else if(a===b ){
+//     console.log("Teng yonli uchburchak")
+// }else {
+//     console.log("Turli tomonli uchburchak")
+// }
+
+// let number =prompt("Enter your number");
+// if (number * 1 === +998) {
+//     console.log("uzbeksan karocci");
+// }else {
+//     console.log("san chetdan kelgan xarpsan")
+// }
